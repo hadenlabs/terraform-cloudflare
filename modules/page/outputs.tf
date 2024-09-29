5edde97abe5f2dde6e60dac6757b82a3d2de911c
Luis Mayta <luis@hadenlabs.com>
2024-09-29 15:13:14 -0300

    chore(page): implement output for domains and subdomain (#4)

--- modules/page/outputs.tf
+++ modules/page/outputs.tf
@@ -2,9 +2,23 @@
 # OUTPUT CALCULATED VARIABLES (prefer full objects)
 # ----------------------------------------------------------------------------------------------------------------------
 
+# pages domain
+#
 output "id" {
   description = "id of page domain"
   value       = local.outputs.module_enabled ? one(cloudflare_pages_domain.this.*.id) : null
+}
+
+# pages project
+
+output "domains" {
+  description = "(List of String) A list of associated custom domains for the project."
+  value       = local.outputs.module_enabled ? one(cloudflare_pages_project.this.*.domains) : null
+}
+
+output "subdomain" {
+  description = "(String) The Cloudflare subdomain associated with the project."
+  value       = local.outputs.module_enabled ? one(cloudflare_pages_project.this.*.subdomain) : null
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
